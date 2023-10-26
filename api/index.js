97d5f7df07ad5a355930853167e7e5638908e58b
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-10-26 23:35:07 +1100

    working on css using dfferent sodaha

--- api/index.js
+++ api/index.js
@@ -1,21 +1,26 @@
-import express from 'express'
-import mongoose from 'mongoose';
-import userRouter from './routes/user.route.js'
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-.then(() => {console.log("Connected to MongoDB!");
-})
-.catch((err) => {
-    console.log(err)
-})
-
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+app.use(express.json());
 
-app.use("/api/user", userRouter)
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import express from "express";
+import { test } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get('/test', test)
+router.get("/test", test);
 export default router;
