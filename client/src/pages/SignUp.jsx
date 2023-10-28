7b783fa80b3504fbc799ab6e7f0a3149c2540deb
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-10-28 23:44:22 +1100

    Error fixed in message error

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -27,7 +27,7 @@ export default function SignUp() {
       const data = await res.json();
       if (data.success === false) {
         setLoading(false);
-        setError(data.messsage);
+        setError(data.message);
         return;
       }
       setLoading(false);
@@ -35,7 +35,7 @@ export default function SignUp() {
       navigate("/sign-in");
     } catch (error) {
       setLoading(false);
-      setError(error.messsage);
+      setError(error.message);
     }
   };
   return (
