660ee5bf83318fe57d8b907ab5bcd669b50423a1
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-11-07 12:17:20 +1100

    updates

---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+
+export default function Listing() {
+  useEffect(() => {
+    const fetchListing = async () => {};
+  });
+  return (
+    <div>
+      <h1>Listing</h1>
+    </div>
+  );
+}
