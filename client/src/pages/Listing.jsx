38607c28ecab55822af3d614ab1284704678bac0
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-11-10 22:28:59 +1100

    complete listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -133,15 +133,9 @@ export default function Listing() {
                 {listing.furnished ? "Furnished" : "Unfurnished"}
               </li>
             </ul>
-            {currentUser && listing.userRef !== currentUser._id && !contact && (
-              <button
-                onClick={() => setContact(true)}
-                className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3"
-              >
-                Contact landlord
-              </button>
-            )}
-            {contact && <Contact listing={listing} />}
+            <button className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3">
+              Contact landlord
+            </button>
           </div>
         </div>
       )}
