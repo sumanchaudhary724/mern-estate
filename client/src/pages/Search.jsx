a948b5c1aecc14e7974ca18246a00b6dbf875e00
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-11-16 23:27:52 +1100

    updates in search pages

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import ListingItem from "../components/ListingItem";
 
 export default function Search() {
   const navigate = useNavigate();
@@ -206,6 +207,21 @@ export default function Search() {
       </div>
       <div className="text-3xl font-semibold border-b p-3 text-slate-700">
         <h1>Listing results:</h1>
+        <div className="p-7 flex flex-wrao gap-4">
+          {!loading && listings.length === 0 && (
+            <p className="text-xl text-slate-700">No listing found!</p>
+          )}
+          {loading && (
+            <p className="text-xl text-slate-700 text-center w-full">
+              Loading...
+            </p>
+          )}
+          {!loading &&
+            listings &&
+            listings.map((listing) => (
+              <ListingItem key={listing._id} listing={listing} />
+            ))}
+        </div>
       </div>
     </div>
   );
