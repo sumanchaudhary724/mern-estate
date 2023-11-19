9802d7e946d8ee611b842395dfe915b1ba15835b
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-11-19 13:55:46 +1100

    fixed the index api path error

--- api/index.js
+++ api/index.js
@@ -18,13 +18,6 @@ mongoose
     console.log(err);
   });
 
-const __dirname = path.resolve();
-
-app.use(express.static(path.join(__dirname, "/client/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
-
 const app = express();
 
 app.use(express.json());
@@ -38,6 +31,13 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
