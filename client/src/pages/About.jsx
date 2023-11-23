921a743626aabafb299fe4cc9d41a0d9d5bd46a6
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-11-23 22:06:04 +1100

    updates in about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -6,7 +6,13 @@ export default function About() {
       <h1 className="text-3xl font-bold mb-4 text-slate-800">
         About Sahand Estate
       </h1>
-
+      <p className="mb-4 text-slate-700">
+        Suman Estate is a leading real estate agency that specializes in helping
+        clients buy, sell, and rent properties in the most desirable
+        neighborhoods. Our team of experienced agents is dedicated to providing
+        exceptional service and making the buying and selling process as smooth
+        as possible.
+      </p>
       <p className="mb-4 text-slate-700">
         Our mission is to help our clients achieve their real estate goals by
         providing expert advice, personalized service, and a deep understanding
